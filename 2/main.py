eeea3f433c1aa7cc20ec2167d931a1e54330b85e
Marco Dalla G <marco.dallagiacoma@gmail.com>
2016-11-16 12:40:52 +0100

    Bonus task

--- 2/main.py
+++ 2/main.py
@@ -12,7 +12,7 @@ def get_all_items(transactions) -> Set:
 
 def apriori(transactions, support_threshold):
     candidates = []  # candidate itemsets of size n
-    frequent = []  # frequent itemsets of size n
+    frequent = []  # frequent itemsets of size n and their support
 
     # put all the possible elements as initial candidates
     # in the form of a set of tuples of a single element
@@ -31,8 +31,8 @@ def apriori(transactions, support_threshold):
                 counter[itemset] = counter.get(itemset, 0) + 1
 
         frequent.append({
-            item for item in counter
-            if counter[item] >= support_threshold
+            itemset: support for itemset, support in counter.items()
+            if support >= support_threshold
             })
 
         level += 1
@@ -51,8 +51,41 @@ def apriori(transactions, support_threshold):
             break
         candidates.append(next_candidates)
 
-    # return candidates, frequent
-    return set.union(*frequent)
+    # # return frequent itemsets
+    # return set.union(*(set(f) for f in frequent))
+
+    # return  frequent itemsets and their support
+    res = {}
+    for f in frequent:
+        res.update(f)
+    return res
+
+
+def compute_association_rules(frequent_itemsets, confidence_threshold):
+    # find all the possible A -> I\A rules, where I is a frequent itemset
+    associations = set()
+    for itemset in frequent_itemsets:
+        # itemsets of size 1 cannot form association rules
+        if len(itemset) == 1:
+            continue
+
+        for i in range(1, len(itemset)):
+            # find all the possible subsets of size i
+            subsets = itertools.combinations(itemset, i)
+
+            # add the association A -> B as a tuple (A, B)
+            for A in subsets:
+                rest = set(itemset) - set(A)
+                t_A = tuple(A)
+                t_rest = tuple(sorted(rest))
+
+                # conf( A -> B ) = support(A U B) / support(A)
+                confidence = frequent_itemsets[itemset] / frequent_itemsets[t_A]
+
+                if confidence >= confidence_threshold:
+                    associations.add((t_A, t_rest))
+
+    return associations
 
 
 def parse_file(path: str) -> List[Set[Any]]:
@@ -68,14 +101,27 @@ def parse_file(path: str) -> List[Set[Any]]:
 
 
 def main():
-    if len(sys.argv) != 3:
-        print("usage: {} <dataset path> <support threshold>".format(sys.argv[0]))
+    if len(sys.argv) != 4:
+        print("usage: {} <dataset path> <support threshold> <confidence threshold>".format(sys.argv[0]))
 
     path = sys.argv[1]
     support_threshold = float(sys.argv[2])
+    confidence_threshold = float(sys.argv[3])
 
     transactions = parse_file(path)
-    print(apriori(transactions, support_threshold))
+    frequent_itemsets = apriori(transactions, support_threshold)
+    association_rules = compute_association_rules(frequent_itemsets, confidence_threshold)
+
+    print("FREQUENT ITEMSETS")
+    print("-----------------")
+    for itemset in frequent_itemsets:
+        print(str(itemset))
+    print("-----------------")
+    print()
+    print("ASSOCIATION RULES")
+    print("-----------------")
+    for assoc_rule in association_rules:
+        print(' -> '.join(str(x) for x in assoc_rule))
 
 if __name__ == '__main__':
     main()
