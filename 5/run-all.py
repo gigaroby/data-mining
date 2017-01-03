6b911cecd8f4748a6e67e3407f9ec84218315eb3
Roberto Bampi <bampi.roberto@gmail.com>
2017-01-03 16:48:40 +0100

    plots

--- 5/run-all.py
+++ 5/run-all.py
@@ -4,33 +4,35 @@ import os
 # alpha, delta, temp, acceptanceFunction, restartAfter
 # 2, 0.003, 2.0, SIMPLE/EXPONENTIAL, 400
 
-def gen_command(af, rounds, alpha, delta, temp, restartAfter):
-    return "./run.sh -acceptanceFunction {} -rounds {} -alpha {} -delta {} -temp {} -restartAfter {}".format(af, rounds, alpha, delta, temp, restartAfter)
 
-rounds = 1000
+def gen_command(graph, af, alpha, delta, temp, restartAfter):
+    return "./run.sh -graph ./graphs/{}.graph -acceptanceFunction {} -alpha {} -delta {} -temp {} -restartAfter {} > /dev/null &".format(graph, af, alpha, delta, temp, restartAfter)
+
 alpha = [2]
-restart_after = [-1, 400, 600]
+restart_after = [-1, 600]
+graphs = ['3elt', 'add20', 'facebook', 'twitter']
 
 def get_command(af, temp, delta):
-    for a in alpha:
-        for ra in restart_after:
-            for d in delta:
-                yield gen_command(af, rounds, a, d, temp, ra)
+    for g in graphs:
+        for a in alpha:
+            for ra in restart_after:
+                for d in delta:
+                    yield gen_command(g, af, a, d, temp, ra)
 
 
 af = 'SIMPLE'
 temp = 2
-delta = [0.003, 0.03, 0.0003]
-
+delta = [0.03]
 
 for s in get_command(af, temp, delta):
     print(s)
 
 af = 'EXPONENTIAL'
 temp = 1
-delta = [0.9, 0.8, 0.99]
+delta = [0.9]
 
 for s in get_command(af, temp, delta):
     print(s)
 
+print("wait")
 print("find output -name \"*.txt\" -exec ./plot.sh {} \;")
--- 5/run-all.sh
+++ 5/run-all.sh
@@ -1,20 +1,18 @@
-./run.sh -acceptanceFunction SIMPLE -rounds 1000 -alpha 2 -delta 0.003 -temp 2 -restartAfter -1
-./run.sh -acceptanceFunction SIMPLE -rounds 1000 -alpha 2 -delta 0.03 -temp 2 -restartAfter -1
-./run.sh -acceptanceFunction SIMPLE -rounds 1000 -alpha 2 -delta 0.0003 -temp 2 -restartAfter -1
-./run.sh -acceptanceFunction SIMPLE -rounds 1000 -alpha 2 -delta 0.003 -temp 2 -restartAfter 400
-./run.sh -acceptanceFunction SIMPLE -rounds 1000 -alpha 2 -delta 0.03 -temp 2 -restartAfter 400
-./run.sh -acceptanceFunction SIMPLE -rounds 1000 -alpha 2 -delta 0.0003 -temp 2 -restartAfter 400
-./run.sh -acceptanceFunction SIMPLE -rounds 1000 -alpha 2 -delta 0.003 -temp 2 -restartAfter 600
-./run.sh -acceptanceFunction SIMPLE -rounds 1000 -alpha 2 -delta 0.03 -temp 2 -restartAfter 600
-./run.sh -acceptanceFunction SIMPLE -rounds 1000 -alpha 2 -delta 0.0003 -temp 2 -restartAfter 600
-./run.sh -acceptanceFunction EXPONENTIAL -rounds 1000 -alpha 2 -delta 0.9 -temp 1 -restartAfter -1
-./run.sh -acceptanceFunction EXPONENTIAL -rounds 1000 -alpha 2 -delta 0.8 -temp 1 -restartAfter -1
-./run.sh -acceptanceFunction EXPONENTIAL -rounds 1000 -alpha 2 -delta 0.99 -temp 1 -restartAfter -1
-./run.sh -acceptanceFunction EXPONENTIAL -rounds 1000 -alpha 2 -delta 0.9 -temp 1 -restartAfter 400
-./run.sh -acceptanceFunction EXPONENTIAL -rounds 1000 -alpha 2 -delta 0.8 -temp 1 -restartAfter 400
-./run.sh -acceptanceFunction EXPONENTIAL -rounds 1000 -alpha 2 -delta 0.99 -temp 1 -restartAfter 400
-./run.sh -acceptanceFunction EXPONENTIAL -rounds 1000 -alpha 2 -delta 0.9 -temp 1 -restartAfter 600
-./run.sh -acceptanceFunction EXPONENTIAL -rounds 1000 -alpha 2 -delta 0.8 -temp 1 -restartAfter 600
-./run.sh -acceptanceFunction EXPONENTIAL -rounds 1000 -alpha 2 -delta 0.99 -temp 1 -restartAfter 600
-
+./run.sh -graph ./graphs/3elt.graph -acceptanceFunction SIMPLE -alpha 2 -delta 0.03 -temp 2 -restartAfter -1 > /dev/null &
+./run.sh -graph ./graphs/3elt.graph -acceptanceFunction SIMPLE -alpha 2 -delta 0.03 -temp 2 -restartAfter 600 > /dev/null &
+./run.sh -graph ./graphs/add20.graph -acceptanceFunction SIMPLE -alpha 2 -delta 0.03 -temp 2 -restartAfter -1 > /dev/null &
+./run.sh -graph ./graphs/add20.graph -acceptanceFunction SIMPLE -alpha 2 -delta 0.03 -temp 2 -restartAfter 600 > /dev/null &
+./run.sh -graph ./graphs/facebook.graph -acceptanceFunction SIMPLE -alpha 2 -delta 0.03 -temp 2 -restartAfter -1 > /dev/null &
+./run.sh -graph ./graphs/facebook.graph -acceptanceFunction SIMPLE -alpha 2 -delta 0.03 -temp 2 -restartAfter 600 > /dev/null &
+./run.sh -graph ./graphs/twitter.graph -acceptanceFunction SIMPLE -alpha 2 -delta 0.03 -temp 2 -restartAfter -1 > /dev/null &
+./run.sh -graph ./graphs/twitter.graph -acceptanceFunction SIMPLE -alpha 2 -delta 0.03 -temp 2 -restartAfter 600 > /dev/null &
+./run.sh -graph ./graphs/3elt.graph -acceptanceFunction EXPONENTIAL -alpha 2 -delta 0.9 -temp 1 -restartAfter -1 > /dev/null &
+./run.sh -graph ./graphs/3elt.graph -acceptanceFunction EXPONENTIAL -alpha 2 -delta 0.9 -temp 1 -restartAfter 600 > /dev/null &
+./run.sh -graph ./graphs/add20.graph -acceptanceFunction EXPONENTIAL -alpha 2 -delta 0.9 -temp 1 -restartAfter -1 > /dev/null &
+./run.sh -graph ./graphs/add20.graph -acceptanceFunction EXPONENTIAL -alpha 2 -delta 0.9 -temp 1 -restartAfter 600 > /dev/null &
+./run.sh -graph ./graphs/facebook.graph -acceptanceFunction EXPONENTIAL -alpha 2 -delta 0.9 -temp 1 -restartAfter -1 > /dev/null &
+./run.sh -graph ./graphs/facebook.graph -acceptanceFunction EXPONENTIAL -alpha 2 -delta 0.9 -temp 1 -restartAfter 600 > /dev/null &
+./run.sh -graph ./graphs/twitter.graph -acceptanceFunction EXPONENTIAL -alpha 2 -delta 0.9 -temp 1 -restartAfter -1 > /dev/null &
+./run.sh -graph ./graphs/twitter.graph -acceptanceFunction EXPONENTIAL -alpha 2 -delta 0.9 -temp 1 -restartAfter 600 > /dev/null &
+wait
 find output -name "*.txt" -exec ./plot.sh {} \;
